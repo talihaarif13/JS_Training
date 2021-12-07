265b9b2391566b0ad4a177dac5b6d471f9d8593f
Taliha Arif <talihaarif@MP-TalihaA-SE-370.local>
2021-12-07 18:06:21 +0500

    day01

---
+++ functions.js
@@ -0,0 +1,9 @@
+function printText() {
+    document.write("hello world");
+}
+function whiteSpaceAdd() {
+    document.write("hello &nbsp world");
+}
+function breakTextLine() {
+    document.write("hello <br/> world");
+}
