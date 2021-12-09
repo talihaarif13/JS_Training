1abd3c07573af3014b5688923d072be08510f9b8
talihaarif13 <talihaarif@MP-TalihaA-SE-370.local>
2021-12-09 18:22:15 +0500

    cookies prompt and alert

--- Day3/functions.js
+++ Day3/functions.js
@@ -1,3 +1,4 @@
+//Grade calculate
 function calculate_grade(){
     let value = document.getElementById('marks').value;
     if(!isNaN(value)){
@@ -18,5 +19,40 @@ function calculate_grade(){
         }
     }else{
         document.write("Not a valid value!");
+    }
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires;
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    console.log(ca);
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+function check_cookie(){
+    let user_name = getCookie("user_name");
+    if(user_name!=""){
+        alert("welcome" + user_name);
+    }else{
+        user_name = prompt("Enter Name");
+        if(user_name!="" && user_name!==null){
+            setCookie("user_name", user_name, 365);
+        }
     }
 }
